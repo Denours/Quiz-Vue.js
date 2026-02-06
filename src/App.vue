e16fc162ca15aca78bdc30da89df02485be7aed7
John Doe <johndoe@exemple.com>
2026-02-06 11:23:23 +0100

    Construction visuelle et logique

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,37 @@
 <template>
-
+  <div class="container">
+    <div v-if="state === 'error'">Impossible de charger le quiz</div>
+    <div v-else>
+      <Quiz :quiz="quiz" v-if="quiz" />
+    </div>
+  </div>
 </template>
 
 <script setup>
+import { onMounted, ref } from "vue";
+import Quiz from "./components/Quiz.vue";
+const quiz = ref(null);
+const state = ref("loading");
+onMounted(() => {
+  fetch("/quiz.json")
+    .then((r) => {
+      if (r.ok) {
+        return r.json();
+      } else {
+        throw new Error("Impossible de récupérer le json");
+      }
+    })
+    .then((data) => {
+      quiz.value = data;
+      state.value = "idle";
+    })
+    .catch((e) => {
+      state.value = "error";
+    });
+});
 </script>
+<style>
+.container {
+  margin-top: 2rem;
+}
+</style>
---
+++ src/components/Question.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="question">
+    <h3>{{ question.question }}</h3>
+    <ul>
+      <li v-for="(choice, index) in question.choices" :key="choice">
+        <label :for="`answer${index}`">
+          <input :id="`answer${index}`" type="radio" name="answer" />
+        </label>
+        {{ choice }}
+      </li>
+    </ul>
+    <button>Question suivante</button>
+  </div>
+</template>
+<script setup>
+const props = defineProps({
+  question: Object,
+});
+</script>
+<style>
+.question button{
+  margin-left: 30%;
+}
+</style>
